583e45a0338848fe333592353e14a904f35066d9
rushi-kesh-19 <111176841+rushi-kesh-19@users.noreply.github.com>
2024-03-24 13:51:12 +0530

    Update deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
 cd Week-5-with-axios-and-backend
- git pull origin master
+ git pull origin main
  cd server
  pm2 kill
  pm2 start index.js
